4029180406819f80d7c03a2aa6fe47278d756839
Estrella Basurto <ebasurto002@ehu.es>
2018-01-03 12:02:12 +0100

    Añadidas funciones a la interfaz de lógica de presentación. Terminadas funciones de la actividad de bienvenida

--- app/src/main/java/eus/ehu/tta/ttaexampleestrella/PresentationIface.java
+++ app/src/main/java/eus/ehu/tta/ttaexampleestrella/PresentationIface.java
@@ -7,4 +7,10 @@ package eus.ehu.tta.ttaexampleestrella;
 public interface PresentationIface {
 
     public abstract boolean authenticateUser(String uName, String psswd);
+    public abstract boolean uploadFile();
+    public abstract boolean takePic();
+    public abstract boolean recordAud();
+    public abstract boolean recordVid();
+    public abstract boolean sendExercise();
+    public abstract String getExWording();
 }
--- app/src/main/java/eus/ehu/tta/ttaexampleestrella/WelcomeActivity.java
+++ app/src/main/java/eus/ehu/tta/ttaexampleestrella/WelcomeActivity.java
@@ -1,15 +1,34 @@
 package eus.ehu.tta.ttaexampleestrella;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class WelcomeActivity extends AppCompatActivity {
 
     public static final String EXTRA_LOGIN = "app_login";
+    private PresentationIface presentation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        TextView welcomeText = (TextView)findViewById(R.id.welcome_text);
+        welcomeText.setText(new StringBuilder().append("Welcome ").append(this.getIntent().getStringExtra(EXTRA_LOGIN)).toString());
+
+
+    }
+    public void nTest(View view){
+        Intent intent = new Intent(this,TestActivity.class);
+        startActivity(intent);
+    }
+    public void nExercise(View view){
+        Intent intent = new Intent(this, ExerciseActivity.class);
+        startActivity(intent);
+    }
+    public void getTracking(View view){
+
     }
 }
