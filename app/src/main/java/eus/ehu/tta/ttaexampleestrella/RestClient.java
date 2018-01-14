3e11ace2e9a9d8d6d27feacaf065d6e2dd0c7419
Estrella Basurto <ebasurto002@ehu.es>
2018-01-14 12:58:39 +0100

    Implementada la clase de cliente REST

--- app/src/main/java/eus/ehu/tta/ttaexampleestrella/RestClient.java
+++ app/src/main/java/eus/ehu/tta/ttaexampleestrella/RestClient.java
@@ -6,9 +6,11 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.HashMap;
@@ -68,13 +70,39 @@ public class RestClient {
     }
 
     public JSONObject getJson(String path) throws IOException , JSONException{
-        JSONObject json = new JSONObject();
+        JSONObject json = new JSONObject(getString(path));
         return json;
     }
 
     public int postFile(String path, InputStream is, String fileName) throws IOException{
-        int i = 0;
-        return i;
+        String boundary = Long.toString(System.currentTimeMillis());
+        String newLine = "\r\n";
+        String prefix = "--";
+
+        HttpURLConnection c = null;
+        try{
+            c = getConnection(path, "POST");
+            c.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
+            c.setDoOutput(true);
+            DataOutputStream out = new DataOutputStream(c.getOutputStream());
+            out.writeBytes(prefix+boundary+newLine);
+            out.writeBytes("Content-Disposition: form-data; name=\"file\"; filename=\"" + fileName + "\"" + newLine);
+            out.writeBytes(newLine);
+            byte[] data = new byte[1024 * 1024];
+            int len;
+            while((len = is.read(data)) > 0){
+                out.write(data,0,len);
+            }
+            out.writeBytes(newLine);
+            out.writeBytes(prefix+boundary+prefix+newLine);
+            out.close();
+            return c.getResponseCode();
+        }
+        finally{
+            if(c != null){
+                c.disconnect();
+            }
+        }
     }
 
     public int postJson(final JSONObject json, String path) throws IOException{
@@ -82,6 +110,12 @@ public class RestClient {
 
         try{
             c = getConnection(path, "POST");
+            c.setRequestProperty("Content-Type","application/json; charset=UTF-8");
+            c.setDoOutput(true);
+            try(PrintWriter pw = new PrintWriter(c.getOutputStream())){
+                pw.print(json.toString());
+                return c.getResponseCode();
+            }
         }finally{
             if(c != null){
                 c.disconnect();
