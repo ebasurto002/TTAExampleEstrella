391d1da0dd8fb88076aaeaceb631377e063e87f5
Estrella Basurto <ebasurto002@ehu.es>
2018-01-17 15:07:24 +0100

    Acabada vista de seguimiento

--- app/src/main/java/eus/ehu/tta/ttaexampleestrella/TrackingActivity.java
+++ app/src/main/java/eus/ehu/tta/ttaexampleestrella/TrackingActivity.java
@@ -2,7 +2,10 @@ package eus.ehu.tta.ttaexampleestrella;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class TrackingActivity extends AppCompatActivity {
@@ -21,7 +24,19 @@ public class TrackingActivity extends AppCompatActivity {
 
             @Override
             protected void onFinish(JSONObject result){
-
+                try {
+                    TextView name = (TextView)findViewById(R.id.uname);
+                    name.setText(result.getString("user"));
+                    TextView lesson = (TextView)findViewById(R.id.title);
+                    lesson.setText(result.getString("lessonTitle"));
+                    TextView nextTest = (TextView)findViewById(R.id.nextTest2Do);
+                    nextTest.setText(result.getString("nextTest"));
+                    TextView nextEx = (TextView)findViewById(R.id.nextEx2Do);
+                    nextEx.setText(result.getString("nextExercise"));
+                }
+                catch(JSONException je){
+                    je.printStackTrace();
+                }
             }
         }.execute();
     }
